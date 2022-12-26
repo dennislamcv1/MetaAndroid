34ef284a122bee5e3aa0c2a709e5ea2148249ee3
Dennis Lam <39684447+dennislamcv1@users.noreply.github.com>
2022-12-26 08:42:23 +0800

    Add files via upload

---
+++ Create the User Interface in Android Studio/LittleLemon Navigation Solution/app/src/main/java/com/example/littlelemon/Destinations.kt
@@ -0,0 +1,13 @@
+package com.example.littlelemon
+
+interface Destinations {
+    val route: String
+}
+
+object Home : Destinations {
+    override val route = "Home"
+}
+
+object Menu : Destinations {
+    override val route = "Menu"
+}
