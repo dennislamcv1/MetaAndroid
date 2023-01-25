8f39bd1e6cb61ad8949e8ea44f5d6b70cac8f7c9
Dennis Lam <39684447+dennislamcv1@users.noreply.github.com>
2023-01-25 15:49:39 +0800

    Add files via upload

---
+++ Working with Data in Android/C7M5 Graded assessment final/app/src/main/java/com/example/littlelemon/Network.kt
@@ -0,0 +1,26 @@
+package com.example.littlelemon
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class MenuNetwork(
+    @SerialName("menu")
+    val menu: List<MenuItemNetwork>,
+)
+
+@Serializable
+data class MenuItemNetwork(
+    @SerialName("id")
+    val id: Int,
+    @SerialName("title")
+    val title: String,
+    @SerialName("price")
+    val price: Double,
+) {
+    fun toMenuItemRoom() = MenuItemRoom(
+        id,
+        title,
+        price
+    )
+}
